7407a868252e63f56fc8df974e19e57ea5500772
DacVuCaMap <namvu04112k@gmail.com>
2024-04-20 20:44:33 +0700

    fix loi

--- src/main/java/com/app/ConStructCompany/Config/SecurityConfig.java
+++ src/main/java/com/app/ConStructCompany/Config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize ->
                         authorize
                                 .requestMatchers("api/auth/login","api/auth/registerbyadmin").permitAll()
-                                .requestMatchers("api/auth/register").hasRole("ADMIN")
+                                .requestMatchers("api/auth/register","api/auth/account").hasRole("ADMIN")
                                 .anyRequest().authenticated()
                 ).authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
